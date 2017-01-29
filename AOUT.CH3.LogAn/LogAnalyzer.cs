528c52fbd72a383643a10dbc4ced0cdab17e5ac4
eugeneborodin <eugeneborodin@mail.ru>
2017-01-29 04:33:40 +0300

    init

---
+++ AOUT.CH3.LogAn/LogAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AOUT.CH3.LogAn
+{
+    public class LogAnalyzer
+    {
+        private bool wasLastFileNameValid;
+
+        public bool WasLastFileNameValid
+        {
+            get { return wasLastFileNameValid; }
+            set { wasLastFileNameValid = value; }
+        }
+
+        public bool IsValidLogFileName(string fileName)
+        {
+            WasLastFileNameValid = false;
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentException("имя файла долно быть задано");
+            }
+
+            if (!fileName.ToLower().EndsWith(".SLF",
+                StringComparison.CurrentCultureIgnoreCase))
+            {               
+                return false;
+            }
+
+            WasLastFileNameValid = true;
+            return true;
+        }
+    }
+}
---
+++ LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+using AOUT.CH3.LogAn;
+
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {        
+        [TestCase("filewithbadextension.SLF", true)]
+        [TestCase("filewithbadextension.slf", true)]
+        [TestCase("filewithbadextension.foo", false)]
+        public void IsValidLogFileName_VariousExtensions_ChecksThem(string file, bool expected)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName(file);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void IsValidFileName_EmptyFileName_Throws()
+        {
+            LogAnalyzer la = MakeAnalyzer();
+
+            var ex = Assert.Catch<Exception>(() => la.IsValidLogFileName(""));
+
+            StringAssert.Contains("имя файла долно быть задано", ex.Message);
+        }
+
+        private LogAnalyzer MakeAnalyzer()
+        {
+            return new LogAnalyzer();
+        }
+
+        [TestCase("badfile.foo", false)]
+        [TestCase("goodfile.slf", true)]
+        public void
+            IsValidFileName_WhenCalled_ChangesWasLastFileNameValid(string file, bool expected)
+        {
+            LogAnalyzer la = new LogAnalyzer();
+
+            la.IsValidLogFileName(file);
+
+            Assert.AreEqual(expected, la.WasLastFileNameValid);
+        }
+    }
+}
